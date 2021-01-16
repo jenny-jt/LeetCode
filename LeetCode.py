185ff4d173f1c6998c464edf7e9d0fb37dd72421
Jenny Tan <tanjenny.yo@gmail.com>
2021-01-15 21:11:38 -0800

    232. Implement Queue using Stacks

--- LeetCode.py
+++ LeetCode.py
@@ -1093,3 +1093,57 @@ def setZeroes(self, matrix: List[List[int]]) -> None:
     while q:
         row, col = q.pop()
         helper(matrix, row, col)
+
+
+######### 232. Implement Queue using Stacks ##############
+class MyQueue:
+    """implement q using 2 stacks"""
+def __init__(self):
+    """
+    Initialize your data structure here.
+    """
+    self.stack1 = []
+    self.stack2 = []
+
+def push(self, x: int) -> None:
+    """
+    Push element x to the back of queue.
+    """
+    self.stack1.append(x)
+
+def pop(self) -> int:
+    """
+    Removes the element from in front of queue and returns that element.
+    """
+    n = len(self.stack1) - 1
+    # move all items except first from stack1 to stack2
+    for i in range(n):
+        self.stack2.append(self.stack1.pop())
+    # return the original first item
+    res = self.stack1.pop()
+    # move the remaining items of the first list
+    for i in range(n):
+        self.stack1.append(self.stack2.pop())
+        
+    return res
+
+def peek(self) -> int:
+    """
+    Get the front element.
+    """
+    n = len(self.stack1) - 1
+    
+    for i in range(n):
+        self.stack2.append(self.stack1.pop())
+        
+    res = self.stack1[0]
+    
+    for i in range(n):
+        self.stack1.append(self.stack2.pop())
+    return res
+
+def empty(self) -> bool:
+    """
+    Returns whether the queue is empty.
+    """
+    return len(self.stack1) == 0

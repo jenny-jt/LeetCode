b02cba22f43973b95696d9b2f18b1300753a2fe8
Jenny Tan <tanjenny.yo@gmail.com>
2021-03-26 11:39:46 -0700

    263. Ugly Number

--- LeetCode.py
+++ LeetCode.py
@@ -4014,7 +4014,21 @@ def twoSum(self, numbers: List[int], target: int) -> List[int]:
             return [i+1, d[num]]
 
 
-#########  ##############
+######### 263. Ugly Number ##############
+def isUgly(self, n: int) -> bool:
+    
+    while n > 1:
+        if n % 2 == 0:
+            n /= 2
+        elif n % 3 == 0:
+            n /= 3
+        elif n % 5 == 0:
+            n /= 5
+        else:
+            return False
+
+    return True
+
 #########  ##############
 #########  ##############
 #########  ##############
